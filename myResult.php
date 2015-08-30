027dbaa7964974233c11fe0b6242fc14b755b105
A. Sya'ban Muhyiddin <a.syaban18@gmail.com>
2015-08-30 21:20:07 +0700

    bikin myResult.php

--- myResult.php
+++ myResult.php
@@ -2,21 +2,21 @@
 if(isset($_GET['submit'])){
 	$nim = @S_GET['nim'];
 	$fname = @$GET('firstname');
-echo "Nama Lengkap ".@fname."<br>";
-echo "Nama Panggilan ".@S_GET['lname']."<br>";
+echo "Nama Lengkap ".@firstame."<br>";
+echo "NIM ".@S_GET['nim']."<br>";
 echo "Jenis Kelamin ".@S_GET['sex']."<br>";
-echo "Alamat ".@S_GET['address']."<br>";
-echo "Pekerjaan  ".@S_GET['pekerjaan']."<br>";
 echo "Kendaraan ".@S_GET['vehicle']."<br>";
+echo "Fakultas ".@S_GET['fakultas']."<br>";
+echo "Alamat  ".@S_GET['alamat']."<br>";
 }elseif(isset($_POST['submit'])){
 	fname = $_POST['fname'];
 	nim = $_POST['nim'];
 echo "Nama Lengkap ".@fname."<br>";
-echo "Nama Panggilan ".@S_POST['lname']."<br>";
+echo "NIM ".@S_POST['lname']."<br>";
 echo "Jenis Kelamin ".@S_POST['sex']."<br>";
-echo "Alamat ".@S_POST['address']."<br>";
-echo "Pekerjaan  ".@S_POST['pekerjaan']."<br>";
 echo "Kendaraan ".@S_POST['vehicle']."<br>";
+echo "Fakultas ".@S_POST['fakultas']."<br>";
+echo "Alamat ".@S_POST['alamat']."<br>";
 }
 ?>
 <html>
