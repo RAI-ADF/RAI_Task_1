7becd722bb5117c6a614544bee7ce33e899b424d
darmayoga <adedarmayoga77@gmail.com>
2015-08-31 23:26:07 +0700

    Update and rename myForm2.html to myForm2.php

---
+++ myForm2.php
@@ -0,0 +1,39 @@
+
+<html>
+<head>
+	<title>Form Get</title>
+</head>
+<body>
+	<h2>Form Get</h2>
+	<form action="myResult.php" method="get">
+		<input type ="hidden" name="nim" value="1103120197"/>
+		Name : <input type ="text" name="name" />
+		</br>
+		</br>
+		Sex : 
+		<input type="radio" name="sex" value="pria" checked>Pria
+		<input type="radio" name="sex" value="wanita">Perempuan
+		</br>
+		</br>
+		Address : 
+		<textarea name="address"></textarea>
+		</br>
+		</br>
+		Class: 
+		<select name="class">
+			<option value="ICM 36 01">ICM 36 01</option>
+			<option value="ICM 36 02">ICM 36 02</option>
+			<option value="ICM 36 03">ICM 36 03</option>
+		</select>
+		</br>
+		</br>
+		Vegetarian? (Check if Yes, ignore if not): 
+		<input name="veget" type="checkbox" value="Yes"/>Yes
+		</br>
+		</br>
+		<input type="submit" value="Proses Data">
+		<input type="reset" value="Reset">
+	</form>
+	<a href="index.html"><button>Back to Index</button></a>
+</body>
+</html>
